db2ad7a6eeda6851fba2c9cb92302672f32e39cd
Faria Jahan <fariaofficial85@gmail.com>
2023-08-31 20:49:14 +0600

    deposite  withdraw

--- js/deposite.js
+++ js/deposite.js
@@ -1,12 +1,11 @@
 //step-1
 document.getElementById('btn-deposit').addEventListener('click',function(){
 //step-2
-
+//id diye input nici,tarpor input take float e convert  
     const depositField=document.getElementById('deposit-field');
-const newDepositAmountString= depositField.value;
-console.log(newDepositAmountString);
- const newDepositAmount=parseFloat(newDepositAmountString);
-  console.log(newDepositAmount);
+    const newDepositAmountString= depositField.value;
+    const newDepositAmount=parseFloat(newDepositAmountString);
+  // console.log(newDepositAmount);
   //step-3
   //input dewar por field ta clean kora
   depositField.value='';
@@ -19,4 +18,12 @@ console.log(newDepositAmountString);
   //step-5
   const newDepositTotal=previousDepositTotal+newDepositAmount;
   depositTotalElement.innerText=newDepositTotal;
+  // step-6
+  const balanceElement = document.getElementById('balance-total');
+  const previousBalanceTotalString = balanceElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+  // step-7
+  const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+  balanceElement.innerText = newBalanceTotal;
 })
